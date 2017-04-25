3a80de53e9a9b5a67719019a6e8c2a366524950f
momo1991817 <1107770992@qq.com>
2017-04-25 16:28:07 +0800

    Update main.js

--- cloud/js/main.js
+++ cloud/js/main.js
@@ -6,33 +6,33 @@
  *  W3C Core Mobile Web Platform Community Group
  *  License: -----
  */
- 
+
 var CoreMobCamera = (function() {
-	
+
 	var maxFilesize = 1048576 * 3.5; // Max image size is 3.5MB (iPhone5, Galaxy SIII, Lumia920 < 3MB)
 	var numPhotosSaved = 0;
 	var imgCrop;
 	var finalPhotoDimension = 612;
 	var viewWidth;
 	var isBlobSupported = true;
-	
+
 	// UI
 	var originalPhoto = document.getElementById('originalPhoto'),
 		resultPhoto = document.getElementById('resultPhoto');
-	
+
 	return {
 		init: init
 	};
-	
+
 	function init() {
 		var prefetchImg = new Image();
 
 		var prefetchImg2 = new Image();
 
-		
+
 		viewWidth = (window.innerWidth < finalPhotoDimension) ? window.innerWidth : finalPhotoDimension;
-		
-		bindEvents();		
+
+		bindEvents();
 
 		checkMediaCaptureSupport();
 		console.log(navigator.userAgent);
@@ -51,12 +51,12 @@ var CoreMobCamera = (function() {
 
 		var index = numPhotosSaved-1;
 		var q = '[data-index="' + index + '"]';
-		
+
 		var oldClone = document.querySelector('.swiper-wrapper');
 		oldClone.parentNode.removeChild(oldClone);
 		/*cloneThumbNode();*/
 	}
-	
+
 	// Note: IE10 Mobile and Maxthon return the window.FileReader as 'function' but fail to read image
 	// I need to write another capability check besides this function
 	function checkMediaCaptureSupport() {
@@ -65,24 +65,24 @@ var CoreMobCamera = (function() {
 			document.querySelector('.camera').classList.add('no-support'); //disable the button
 		}
 	}
-	
+
 	function checkHistorySupport() {
 		if (typeof history.pushState === 'undefined') {
 			showUI(document.getElementById('warningHistory'));
 		}
 	}
-	
-    function renderFirstRun() {
 
-	    var arrowHeight = window.innerHeight * .5;
+	function renderFirstRun() {
+
+		var arrowHeight = window.innerHeight * .5;
 		document.getElementsByClassName('arrow-container')[0].style.height = arrowHeight + 'px';
-    }
-       
+	}
+
 	function bindEvents() {
 		// Screen orientation/size change
 		var orientationEvent = ('onorientationchange' in window) ? 'orientationchange' : 'resize';
 		window.addEventListener(orientationEvent, function() {
-		    /*displayThumbnails();*/
+			/*displayThumbnails();*/
 		}, false);
 
 		// A photo taken (or a file chosen)
@@ -97,63 +97,64 @@ var CoreMobCamera = (function() {
 	function cropAndResize() {
 		var photoObj = document.getElementById('originalPhoto');
 
-	    imgCrop = new PhotoCrop(photoObj, {
+		imgCrop = new PhotoCrop(photoObj, {
 			size: {w: finalPhotoDimension, h: finalPhotoDimension}
-	    });
-	    
+		});
+
 		var newImg = imgCrop.getDataURL();
 		imgCrop.displayResult();
 		document.getElementById('croppedPhoto').setAttribute('hidden','hidden'); //keep in DOM but not shown
-		
+
 		resultPhoto.src = newImg;
-		resultPhoto.style.width = resultPhoto.style.height = viewWidth +'px';
-		
+		resultPhoto.style.width = 310 + 'px';
+		resultPhoto.style.height = 230 +'px';
+
 		// Removing the previously created canvas, if any
 		var prevEffect = document.getElementById('filteredPhoto');
-		if(prevEffect) {	
+		if(prevEffect) {
 			prevEffect.parentNode.removeChild(prevEffect);
 
 		}
 
 		history.pushState({stage: 'effectView'}, null);
 	}
-	
+
 	/**
 	 * File Picker
 	 */
 
 	function fileSelected(capture) {
-		
-	    var localFile = document.getElementById(capture).files[0],
-	    	imgFormat = /^(image\/bmp|image\/gif|image\/jpeg|image\/png)$/i;
-	    	
-	    if (! imgFormat.test(localFile.type)) {
-	        alert('The image format, ' + localFile.type + ' is not supported.');
-	        return;
-	    }
-	    
-	    if (localFile.size > maxFilesize) { //this should exclude a huge panorama pics
-	        alert('The file size is too large.');
-	        return;
-	    }
-	    
+
+		var localFile = document.getElementById(capture).files[0],
+			imgFormat = /^(image\/bmp|image\/gif|image\/jpeg|image\/png)$/i;
+
+		if (! imgFormat.test(localFile.type)) {
+			alert('The image format, ' + localFile.type + ' is not supported.');
+			return;
+		}
+
+		if (localFile.size > maxFilesize) { //this should exclude a huge panorama pics
+			alert('The file size is too large.');
+			return;
+		}
+
 		// display the selected image
-	    var orig = document.getElementById('originalPhoto');
-	    var imgFile = new FileReader();
-	    
+		var orig = document.getElementById('originalPhoto');
+		var imgFile = new FileReader();
+
 		imgFile.onload = function(e){
-	        // e.target.result contains the Base64 DataURL
-	        orig.onload = function () {
-	        	cropAndResize();	        
+			// e.target.result contains the Base64 DataURL
+			orig.onload = function () {
+				cropAndResize();
 				//displayFileInfo(localFile, orig);
-	        };
-	        orig.src = e.target.result;
-	    };
-		
-	    imgFile.readAsDataURL(localFile);
+			};
+			orig.src = e.target.result;
+		};
+
+		imgFile.readAsDataURL(localFile);
 	}
 
-	
+
 }());
 
 onload = function() {
